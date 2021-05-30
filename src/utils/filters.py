efcf81b7342c3d4298ff54382e1548e614aaa327
m4xx1m <semeika2280@gmail.com>
2021-05-30 20:51:31 +0300

    added filters.py

---
+++ src/utils/filters.py
@@ -0,0 +1,17 @@
+import json
+from aiogram.dispatcher.filters import BoundFilter
+from aiogram import types
+from .db import DataBase
+
+
+class AdminFilter(BoundFilter):
+    key = 'is_admin'
+
+    def __init__(self, is_admin):
+        self.is_admin = is_admin
+
+    async def check(self, message: types.Message):
+        if not self.is_admin:
+            return True
+        return message.from_user.id in message.bot.bot_admins
+
